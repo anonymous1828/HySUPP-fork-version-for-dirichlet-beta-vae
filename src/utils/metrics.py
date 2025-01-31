ba81a0a71621411f77031cbfa7156258eb33dcb9
anonymous1828 <197140653+anonymous1828@users.noreply.github.com>
2025-01-31 17:54:47 +0100

    GammaKL added in metrics

--- src/utils/metrics.py
+++ src/utils/metrics.py
@@ -209,3 +209,69 @@ class SREAggregator(RunAggregator):
             use_endmembers=False,
             detail=False,
         )
+
+
+
+import torch
+import torch.nn as nn
+
+
+class GammaKL:
+    """
+        KL divergence closed form for mGamma distrib from
+        "Dirichlet Variational Autoencoder"by
+        Weonyoung Joo, Wonsung Lee, Sungrae Park & Il-Chul Moon
+
+        Analytical form:
+        Let P and Q be 2 multiGamma distributions of parameters
+        $\alpha, \beta$ (resp. $\alpha, \beta$) where $\sum_{i=1}^K \alpha_i = 1$
+        and $\beta > 0$, (resp. $\sum_{i=1}^K \hat \alpha_i =1 \text{ and } \beta > 0$).
+        Based on the paper the analytical form is derived as follows
+        $$
+            D_{KL} (Q \| P) =   \sum_{i=1}^K log(\Gamma(\hat \alpha_i)) \\
+                              - \sum_{i=1}^K log( \Gamma( \alpha_i ) )
+                              + \sum_{i=1}^K ( \hat \alpha_i - \alpha_i ) \psi(\hat \alpha_i)
+        $$
+        where $\psi(\cdot)$ is the derivative of the gamma function (digamma function).
+    """
+    def __init__(self,
+                 alphas: torch.Tensor,
+                 reduction: str = "sum"):
+        """
+            Provide alphas of target distribution
+            (betas is assumed to be the same as target)
+        """
+        self.alphas    = alphas.to(dtype=torch.float32)
+        self.reduction = reduction
+
+    def to(self, device):
+        self.alphas = self.alphas.to(device)
+        return self
+
+    def __repr__(self):
+        return f"GammaKL({self.alphas}, beta assumed to be the same as target)"
+
+    def __call__(self,
+                 input: torch.Tensor):
+        """
+            Args:
+                input:  moments of shape (batch_size, n_ems)
+            Returns:
+                output: shape (batch_size,)
+        """
+        #alphas shape (1, x) --> shape (batch_size, x)
+        batch_size = input.shape[0]
+        alphas = self.alphas.expand(batch_size, -1)
+
+        loss  = torch.sum(torch.lgamma(alphas), dim=1)
+        loss -= torch.sum(torch.lgamma(input), dim=1)
+        loss += torch.sum((input - alphas) * torch.digamma(input), dim=1)
+
+        if   self.reduction == "sum":  loss = loss.sum()
+        elif self.reduction == "mean": loss = loss.mean()
+        elif self.reduction == "none": pass
+        else:
+            raise ValueError("Invalid reduction mode")
+
+        return loss
+
